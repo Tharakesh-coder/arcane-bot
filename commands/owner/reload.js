e28a64ff17c5e721a7c71df336cfd72a0e9a86c4
lilmonkeylv <62300189+lilmonkeylv@users.noreply.github.com>
2021-08-26 01:52:58 +0300

    Update reload.js

--- commands/owner/reload.js
+++ commands/owner/reload.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const dateNow = moment(new Date()).format("LTS");
 
 module.exports.run = async (bot, message, args) => {
-    message.delete();
+    message.delete(s);
     
     // console.error(chalk`{gray [${dateNow}]} {redBright ERROR:}`, "tirlins 123")
     new TypeError("tirlins123");
