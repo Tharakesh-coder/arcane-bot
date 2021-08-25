c683571bea3c28c3219643ae55d1e128c0504b62
lilmonkeylv <62300189+lilmonkeylv@users.noreply.github.com>
2021-08-26 01:35:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const bot = new Client({
     partials: ['MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'REACTION'],
     intents: 32767,
 });
+const moment = require('moment');
+const dateNow = moment(new Date()).format("LTS");
 
 /* Configs */
 bot.config = require('./configs/config.json');
 bot.colors = require('./configs/colors.json');
+
+bot.on('error', async (error) => {
+    console.error(chalk`{gray [${dateNow}]} {redBright ERROR:}`, error);
+});
 
 /* Load commands/events */
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
