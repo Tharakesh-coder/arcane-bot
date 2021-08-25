61d883c5f288cf4232c6804c4d1018db83ca8038
lilmonkeylv <62300189+lilmonkeylv@users.noreply.github.com>
2021-08-26 02:12:40 +0300

    Fixed error logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require("discord.js");
+const startLogger = require('./functions/cleanups/loggerFunctions');
 const { token } = require("./configs/config.json");
 const bot = new Client({ 
     partials: ['MESSAGE', 'CHANNEL', 'GUILD_MEMBER', 'REACTION'],
@@ -8,20 +9,7 @@ const bot = new Client({
 /* Configs */
 bot.config = require('./configs/config.json');
 bot.colors = require('./configs/colors.json');
-
-const winston = require('winston');
-const chalk = require('chalk');
-const moment = require('moment');
-const dateNow = moment(new Date()).format("LTS");
-
-/* Configs */
-bot.logger = winston.createLogger({
-	transports: [ 
-		new winston.transports.Console(),
-	],
-	format: winston.format.printf(log => chalk`{gray [${dateNow}]} {redBright ${log.level.toUpperCase()}:} ${log.message}`),
-});
-// process.on('uncaughtException', (error) => bot.logger.log('error', error.stack));
+startLogger(bot);
 
 /* Load commands/events */
 ["aliases", "commands"].forEach(x => bot[x] = new Collection());
