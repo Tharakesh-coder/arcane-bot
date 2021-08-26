0d3ba75757d71636a299adbc01a60a6151bc99bc
lilmonkeylv <62300189+lilmonkeylv@users.noreply.github.com>
2021-08-26 03:07:14 +0300

    Update exit.js

--- events/process/exit.js
+++ events/process/exit.js
@@ -4,11 +4,11 @@ const dateNow = moment(new Date()).format("LTS")
 const log = console.log;
 
 module.exports = (e) => {
-       log(chalk`\n{gray [${dateNow}]} {red Activating shutdown sequence}{rgb(255,255,255) ...}`);
-       log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Closing threads...}`);
-       log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Unloading commands...}`);
-       log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Done!}`);
-       log(chalk`{gray [${dateNow}]} {red Shutting down}{rgb(255,255,255) ...}`);
-       log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Thank you and goodbye!}`);
-       log(chalk`{gray [${dateNow}]} {red Shutdown successful}{rgb(255,255,255) !}\n`);
+    log(chalk`\n{gray [${dateNow}]} {red Activating shutdown sequence}{rgb(255,255,255) ...}`);
+    log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Closing threads...}`);
+    log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Unloading commands...}`);
+    log(chalk`{gray [${dateNow}]} {rgb(255,255,255) Done!}`);
+    log(chalk`{gray [${dateNow}]} {red Shutting down}{rgb(255,255,255) ...}`);
+    log(chalk`{gray [${dateNow}]} {red Shutdown successful}{rgb(255,255,255) !}\n`);
+    log(chalk`{gray [${dateNow}]} --- {rgb(255,255,255) Thank you and goodbye!} ---`);
 };
